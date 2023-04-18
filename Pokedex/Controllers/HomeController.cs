aa2262bce76afe522064778b06d973ee1dddc054
Guilherme Henrique <guizinho.zaza14@gmail.com>
2023-04-18 08:48:39 -0300

    Criando Service

---
+++ Pokedex/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.Json;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Pokedex.Models;
+using Pokedex.Services;
+
+namespace Pokedex.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly IPokeService _pokeService;
+
+    public HomeController(ILogger<HomeController> logger, IPokeService service)
+    {
+        _logger = logger;
+        _pokeService = pokeService;
+    }
+
+    public IActionResult Index(string tipo)
+    {
+        var pokes = _pokeService.GetPokedexDto();
+        ViewData["filter"] = string.IsNullOrEmpty(tipo) ? "all" : tipo;
+        return View(pokes);
+    }
+
+    public IActionResult Details(int Numero)
+    {
+        var pokemon = _pokeService.GetDetailedPokemon(Numero);
+        return View(pokemon);
+    }
+
+    
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Services/IPokeService.cs
@@ -0,0 +1,13 @@
+using Pokedex.Models;
+
+namespace Pokedex.Services;
+
+public interface IPokeService
+{
+    List<Pokemon> GetPokemons();
+    List<Tipo> GetTipos();
+    Pokemon GetPokemon(int Numero);
+    PokedexDto GetPokedexDto();
+    DetailsDto GetDetailedPokemon(int Numero);
+    Tipo GetTipo(string Nome);
+}
